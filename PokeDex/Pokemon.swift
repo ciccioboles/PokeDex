4ee79956384a2616ef14e89f5015c9464ed21aa9
dev <davidboles@davids-MacBook-Pro.local>
2016-01-19 18:15:14 -0800

    second or third commit

---
+++ PokeDex/Pokemon.swift
@@ -0,0 +1,47 @@
+//
+//  Pokemon.swift
+//  PokeDex
+//
+//  Created by dev on 1/19/16.
+//  Copyright © 2016 ciccio boles. All rights reserved.
+//
+
+import Foundation
+
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}//
