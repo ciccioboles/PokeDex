951d20d10de142f5c2fd7f111002ec7864c90bfc
dev <davidboles@davids-MacBook-Pro.local>
2016-01-19 18:41:05 -0800

    fourth commit

--- PokeDex/ViewController.swift
+++ PokeDex/ViewController.swift
@@ -13,15 +13,29 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     @IBOutlet weak var collection: UICollectionView!
     
+    var pokemon = [Pokemon]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collection.delegate = self
         collection.dataSource = self
         
-        
+        parsePokemonCSV()
     }
     
+    func parsePokemonCSV() {
+        let path = NSBundle.mainBundle().pathForResource("pokemon", ofType: "csv")!
+        
+        do {
+            let csv = try CSV(contentsOfURL: path)
+            let rows = csv.rows
+            print(rows)
+        }catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+    }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell {
